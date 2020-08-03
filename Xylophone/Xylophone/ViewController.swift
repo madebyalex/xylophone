3dcc12698a6726fd212b634a3e947bb21fea5506
Alexander <hello@alexander.works>
2020-08-04 00:57:08 +0300

    Binding the local and external versions

---
+++ Xylophone/Xylophone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Alexander on 04.08.2020.
+//  Copyright © 2020 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
