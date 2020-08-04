cd6e3a39a9e611394590fbcb2f30e0de635bfe15
Alexander <hello@alexander.works>
2020-08-04 13:44:02 +0300

    Ready: Playing different sounds driven by a single function.

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -7,16 +7,26 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+       
+        playSound(soundName: sender.currentTitle!)
     }
     
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: "Sounds/\(soundName)", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+//        print("Sounds/" + title)
+    }
 }
 
